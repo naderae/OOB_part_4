520b5e3c17943605592dff21771d133ed5d4331e
naderae <naderabouezze93@gmail.com>
2017-07-07 14:31:46 -0400

    started solar system

---
+++ solar_system.rb
@@ -0,0 +1,44 @@
+  class System
+
+    @@bodies = []
+
+    def initialize
+
+    end
+
+    def bodies
+      @@bodies
+    end
+
+    def add(celestial_body)
+      @@bodies << celestial_body
+    end
+
+    def total_mass
+      sum = 0
+      @@bodies.each do |body|
+      # not done this yet!!!
+
+
+    end
+
+
+  end
+
+  class Body
+
+    def initialize
+    @name = name
+    @mass = mass
+
+
+    end
+
+
+
+
+
+
+
+
+  end
