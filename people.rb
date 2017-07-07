7c0dad06913a791338e6d0e5329ad19ff29b4b2a
naderae <naderabouezze93@gmail.com>
2017-07-07 13:55:03 -0400

    second

--- people.rb
+++ people.rb
@@ -1,9 +1,19 @@
-class Student
+class Person
 
   def initialize(name)
     @name = name
   end
 
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
+end
+
+
+
+
+
+class Student < Person
 
 
   def learn
@@ -13,12 +23,38 @@ class Student
 end
 
 
-
-
-class Instructor
+class Instructor < Person
 
   def teach
     puts "Everything in Ruby is an object"
   end
 
 end
+
+chris = Instructor.new("Chris")
+chris.greeting
+christina = Student.new("Christina")
+christina.greeting
+
+chris.teach
+
+
+class Instructor < Person
+
+  def teach
+    puts "Everything in Ruby is an object"
+  end
+
+end
+
+chris = Instructor.new("Chris")
+chris.greeting
+christina = Student.new("Christina")
+christina.greeting
+
+chris.teach
+christina.learn
+
+christina.teach
+# this doesnt work because christina is not an Instructor, which is the class that includes the teach method.
+# to make it work, you can create a teach class in the class person.
