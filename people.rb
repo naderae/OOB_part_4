a17b001e3866559fa9e32d1db057a481f434c15c
naderae <naderabouezze93@gmail.com>
2017-07-07 13:42:33 -0400

    1st

--- people.rb
+++ people.rb
@@ -1,10 +1,24 @@
 class Student
 
+  def initialize(name)
+    @name = name
+  end
+
+
+
+  def learn
+    puts "I get it!"
+  end
+
 end
 
 
 
 
 class Instructor
+
+  def teach
+    puts "Everything in Ruby is an object"
+  end
 
 end
